1b29b84248e49a3923b27d31e0831b2dc8aa0d18
Twolf59 <tomdsx.59118@gmail.com>
2021-10-03 21:19:59 +0200

    index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    IndexComponent
   ],
   imports: [
     BrowserModule,
