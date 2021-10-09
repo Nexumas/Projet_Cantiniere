6f58b9b185865af2e0d9b17a5076174fa389435e
Twolf59 <tomdsx.59118@gmail.com>
2021-10-09 22:06:04 +0200

    fix conflict merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
@@ -30,29 +29,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-=======
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { IndexComponent } from './index/index.component';
-import {PanierComponent} from './panier/panier.component';
-
-const routes: Routes = [
-
-  {
-    path : '',
-    component : IndexComponent,
-
-  },
-  {
-    path : 'panier',
-    component : PanierComponent,
-  }
-
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
->>>>>>> feature-panier
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -30,29 +29,3 @@ import { PanierComponent } from './panier/panier.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-=======
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import {PanierComponent} from "./panier/panier.component";
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    IndexComponent,
-    NavbarComponent,
-    PanierComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
->>>>>>> feature-panier
