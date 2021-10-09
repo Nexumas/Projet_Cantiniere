32a317774d90d8305dca7de799d8bb7dbcbef4ce
Twolf59 <tomdsx.59118@gmail.com>
2021-10-09 19:12:40 +0200

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import {MdpoublieComponent} from './mdpoublie/mdpoublie.component';
+import {NavbarComponent} from "./navbar/navbar.component";
+import {IndexComponent} from "./index/index.component";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent,
-    NavbarComponent
+    LoginComponent,
+    MdpoublieComponent,
+    NavbarComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
@@ -20,3 +25,4 @@ import { NavbarComponent } from './navbar/navbar.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
